bd21c935104a7386818243a2d17c986d2b9c75fa
kewlsak <steven.kraemer@gmail.com>
2016-02-17 04:14:19 +0000

    Created new Tk container for images

---
+++ TkPictureFrame.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+
+class TkPictureFrame(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.picture = Label(self)
+        self.picture.grid(row=0,column=0)
+
+    def changePic(self, photo):
+        self.photo = photo
+        self.picture["image"] = self.photo
+
+
+if __name__ == "__main__":
+    root = Tk()
+      
+    pictureframe = TkPictureFrame(root)
+    pictureframe.grid(row=0, column=0)
+    root.mainloop()
+    
