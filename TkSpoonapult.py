d5567f30f7363555ddd8b8105d16a94bf17283ab
kewlsak <steven.kraemer@gmail.com>
2016-02-18 05:45:10 +0000

    Added picture to TkSpoonapult

--- TkSpoonapult.py
+++ TkSpoonapult.py
@@ -1,10 +1,15 @@
 from TkCatapultControl import *
+from TkPictureFrame import *
 from ServoCatapult import *
 
 if __name__ == "__main__":
     root = Tk()
-    catapult = SimpleCatapult()
-    #catapult = ServoCatapult()
+
+    picture = TkPictureFrame(400, 300, root)
+    picture.grid(row=0, column=0)
+    
+    #catapult = SimpleCatapult()
+    catapult = ServoCatapult()
     catapult.DEBUG = True
     control = TkCatapultControl(catapult, root)
     control.grid(row=1, column=0)
