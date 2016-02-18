4fd282f99516c50a3ae8da4af4e9a1e3b4e1c7a2
kewlsak <steven.kraemer@gmail.com>
2016-02-18 05:46:41 +0000

    Remove debug messages from TkSpoonapult

--- TkSpoonapult.py
+++ TkSpoonapult.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     
     #catapult = SimpleCatapult()
     catapult = ServoCatapult()
-    catapult.DEBUG = True
+    #catapult.DEBUG = True
     control = TkCatapultControl(catapult, root)
     control.grid(row=1, column=0)
     root.mainloop()
