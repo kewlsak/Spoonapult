461f8073bb343488827a2a54e82c166f7d690989
kewlsak <steven.kraemer@gmail.com>
2016-02-19 05:25:59 +0000

    Add camera toggle to pic frame

--- TkPictureFrame.py
+++ TkPictureFrame.py
@@ -1,4 +1,4 @@
-from Tkinter import Tk, Frame, Canvas
+from Tkinter import Tk, Frame, Canvas, Checkbutton
 import picamera
 import io
 import thread
@@ -8,6 +8,7 @@ class TkPictureFrame(Frame):
     def __init__(self, x, y, master=None):
         Frame.__init__(self, master)
         self.photo = None
+        self.resolution = (x, y)
         #The center of the Canvas is 0, 0. Find the center so
         #we can draw the image properly.
         self.center = ( x/2, y/2)
@@ -15,8 +16,14 @@ class TkPictureFrame(Frame):
         self.picture = Canvas(self, width=x, height=y)
         #Place the canvas in the Grid.
         self.picture.grid(row=0,column=0)
-        #Start the picamera capture thread.
-        thread.start_new_thread(self.setupCamera, (x,y))
+
+        self.checkButton = Checkbutton(self, text='Camera?',\
+            command=self.toggleCamera)
+        self.checkButton.grid(row=1,column=0)
+
+        #To determine if the camera is running
+        self.capturing = False
+       
 
     def changePic(self, photo):
         #Make a reference to the old photo for removal
@@ -27,7 +34,15 @@ class TkPictureFrame(Frame):
         self.picture.create_image(self.center,image=self.photo)
         #Remove the old photo
         self.picture.delete(self.oldphoto)
-        
+            
+
+    def toggleCamera(self):
+        if self.capturing:
+            self.capturing = False
+            self.picture.delete("all")
+        else:
+            thread.start_new_thread(self.setupCamera, self.resolution)
+            self.capturing = True
 
     def setupCamera(self, x, y):
         with picamera.PiCamera() as camera:
@@ -42,9 +57,14 @@ class TkPictureFrame(Frame):
                 #Open the image stream and rotate is 180 degrees
                 image = Image.open(each).transpose(Image.ROTATE_180)
                 photo = ImageTk.PhotoImage(image)
+                #Break out of the loop if not capturing
+                if not self.capturing:
+                    break
+                #Update the canvas
                 self.changePic(photo)
                 #Reset playback to the beginning for the next image.
                 each.seek(0)
+                
                 
                 
                 
@@ -55,5 +75,7 @@ if __name__ == "__main__":
       
     pictureframe = TkPictureFrame(400, 300, root)
     pictureframe.grid(row=0, column=0)
+    
+    
     root.mainloop()
     
